a8f6b2d62850c600c2dc37189f80a6f98183a020
mkdirprince <mkdirprince@github.com>
2025-04-04 15:09:11 +0000

    method to print the graph to the command line

--- labs/lab1/src/Main.java
+++ labs/lab1/src/Main.java
@@ -46,6 +46,32 @@ public class Main {
         return (double) sumOfScores / scores.length;
     }
 
+    // prints the graph
+    public static void printGraph(int[] stats) {
+        // maximum frequency
+        int maxStat = maxGrade(stats);
+
+        // Print the graph from top to bottom
+        for (int i = maxStat; i > 0; i--) {
+            System.out.printf("%2d  > ", i); // Print the current count number
+            for (int stat : stats) {
+                // Print '#####' for each column where the count is greater than or equal to the current level
+                if (stat >= i) {
+                    System.out.printf("%-10s", "    #####");
+
+                } else {
+                    // Otherwise print spaces to keep the columns aligned
+                    System.out.printf("%-10s", "     ");
+                }
+            }
+            System.out.println(); // Move to the next row
+        }
+
+        // Print the base of the graph (the grade intervals)
+        System.out.println("     +-----------+---------+---------+---------+---------+");
+        System.out.println("     I    0-20   I   21-40 I  41-60  I  61-80  I  81-100 I");
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
