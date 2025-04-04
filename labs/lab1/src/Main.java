6a7abf4d0e8c10ef89b153de15a3438a0989201f
mkdirprince <mkdirprince@github.com>
2025-04-04 15:06:41 +0000

    method to compute max grade

--- labs/lab1/src/Main.java
+++ labs/lab1/src/Main.java
@@ -1,4 +1,18 @@
 public class Main {
+
+    // Returns the maximum grade
+    public static int maxGrade(int[] scores) {
+        int max = scores[0];
+
+        for (int num : scores) {
+            if (num > max) {
+                max = num;
+            }
+        }
+
+        return max;
+    }
+    
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
