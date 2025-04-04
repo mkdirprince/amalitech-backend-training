5054b781c376ddb733839af7f0637060f0e3c68e
mkdirprince <mkdirprince@github.com>
2025-04-04 15:07:06 +0000

    method to compute min grade

--- labs/lab1/src/Main.java
+++ labs/lab1/src/Main.java
@@ -12,7 +12,21 @@ public class Main {
 
         return max;
     }
-    
+
+    // Returns the minimum grade
+    public static int minGrade(int[] scores) {
+
+        int min = scores[0];
+
+        for (int num : scores) {
+            if (num < min) {
+                min = num;
+            }
+        }
+
+        return min;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
