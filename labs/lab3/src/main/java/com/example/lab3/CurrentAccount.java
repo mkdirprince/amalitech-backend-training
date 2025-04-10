ec738f816cae33ea2bfe6577facfaf678f09709c
mkdirprince <mkdirprince@github.com>
2025-04-10 17:31:17 +0000

    implement currnt account subclass

---
+++ labs/lab3/src/main/java/com/example/lab3/CurrentAccount.java
@@ -0,0 +1,39 @@
+package com.example.lab3;
+
+public class CurrentAccount extends BankAccount {
+    private static final double OVERDRAFT_LIMIT = 100.00;
+
+    //constructor with fixed overdraft
+    public CurrentAccount(String accountName, double initialBalance, String type) {
+        super(accountName, initialBalance, type);
+    }
+
+    // getter for overdraft limit
+    public double getOverdraftLimit() {
+        return OVERDRAFT_LIMIT;
+    }
+
+    @Override
+    public void deposit(double amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Deposit amount must be greater than zero.");
+        }
+
+        updateBalance(amount);
+        addTransaction(amount, "DEBIT");
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Withdrawal amount must be greater than zero.");
+        }
+
+        if (getBalance() - amount < -OVERDRAFT_LIMIT) {
+            throw new IllegalStateException("Insufficient funds. Balance exceeded overdraft limit");
+        }
+
+        updateBalance(-amount);
+        addTransaction(amount, "CREDIT");
+    }
+}
