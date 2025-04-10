7b24118b113a69367c25d40b8d5dda2a3b3278b4
mkdirprince <mkdirprince@github.com>
2025-04-10 17:29:16 +0000

    implement abstract class for bank account

---
+++ labs/lab3/src/main/java/com/example/lab3/BankAccount.java
@@ -0,0 +1,74 @@
+package com.example.lab3;
+
+import java.util.UUID;
+
+public abstract class BankAccount implements BankOperations {
+    private static int accountCounter = 1000000; // To start numbering accounts from this point
+    private final String accountHolderId;
+
+    protected String accountHolder;
+    protected String accountType;
+    protected String accountNumber;
+    protected double balance;
+    protected SLList transactionHistory;
+
+
+    // constructor
+    public BankAccount(String accountName, double initialBalance, String type) {
+        this.accountHolder = accountName;
+        this.accountType = type;
+        this.accountNumber = generateAccountNumber();
+        this.accountHolderId = String.format("%s-%s", type, UUID.randomUUID().toString());
+        this.balance = initialBalance;
+        this.transactionHistory = new SLList(new Transaction(initialBalance, "INITIAL"));
+    }
+
+    // Method to generate unique account number
+    private String generateAccountNumber() {
+        return String.format("010%d02", accountCounter++);
+    }
+
+    //getters
+
+    // returns the name of the account holder
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    // returns the ID of the account holder
+    public String getAccountHolderId() {
+        return accountHolderId;
+    }
+
+    // returns the account number
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    // returns the account type
+    public String getAccountType() {
+        return accountType;
+    }
+
+    //returns the account balance
+    public double getBalance() {
+        return balance;
+    }
+
+
+    public int getDuration() {
+        return 0; // Default for non-fixed accounts
+    };
+
+    public void updateBalance(double amount) {
+        this.balance += amount;
+    }
+
+    public void addTransaction(double amount, String type) {
+        this.transactionHistory.addFirst(new Transaction(amount, type));
+    }
+
+    public abstract void deposit(double amount);
+    public abstract void withdraw(double amount);
+
+}
