21574d48591ed627c57bca8c5f1e31eb813b3439
mkdirprince <mkdirprince@github.com>
2025-04-10 21:08:56 +0000

    rename the BankApplication

--- labs/lab3/src/main/java/com/example/lab3/BankApplicationUI.java
+++ labs/lab3/src/main/java/com/example/lab3/BankApplicationUI.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class BankApplication extends Application {
+public class BankApplicationUI extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(BankApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(BankApplicationUI.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 720, 540);
         stage.setTitle("Hello!");
         stage.setScene(scene);
