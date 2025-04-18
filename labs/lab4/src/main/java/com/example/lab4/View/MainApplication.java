c6435b08c92917b819b15a1661c5d61d78fe11dd
mkdirprince <mkdirprince@github.com>
2025-04-18 05:33:51 +0000

    change size and add title

--- labs/lab4/src/main/java/com/example/lab4/View/MainApplication.java
+++ labs/lab4/src/main/java/com/example/lab4/View/MainApplication.java
@@ -11,8 +11,8 @@ public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/lab4/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 860, 600);
+        stage.setTitle("Employee Management System!");
         stage.setScene(scene);
         stage.show();
     }
