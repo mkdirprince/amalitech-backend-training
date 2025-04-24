66e65d9795a3dd2180aae47ab9f2f101f34e3c2d
mkdirprince <mkdirprince@github.com>
2025-04-24 15:04:41 +0000

    configure imports and dependencies to work with tests

--- labs/lab4/src/main/java/module-info.java
+++ labs/lab4/src/main/java/module-info.java
@@ -1,10 +1,12 @@
 module com.example.lab4 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.logging;
 
     opens com.example.lab4.Controller to javafx.fxml;
 
     exports com.example.lab4.Model;
     exports com.example.lab4.View;
     exports com.example.lab4.Utils;
+    exports com.example.lab4.Exceptions;
 }
